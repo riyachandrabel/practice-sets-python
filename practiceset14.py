98338eb6a64713b7d03512d472d30f518a05cd9f
Riya Chandrabel <158443536+riyachandrabel@users.noreply.github.com>
2024-04-08 10:41:04 +0530

    Delete practiceset14.py

--- practiceset14.py
+++
@@ -1,9 +0,0 @@
-# calculator
-
-a=int(input("enter first number:"))
-b=int(input("enter second number:"))
-print("sum of",a,"and",b,"is",a+b)
-print("difference of",a,"and",b,"is",a-b)
-print("procuct of",a,"and",b,"is",a*b)
-print("division of",a,"and",b,"is",a/b)
-print("remainder of two no. is",a%b)
