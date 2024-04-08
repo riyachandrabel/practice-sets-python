ac73b0221028670239eab58f3aed723bae96759c
Riya Chandrabel <158443536+riyachandrabel@users.noreply.github.com>
2024-04-08 10:41:58 +0530

    Delete practiceset13.py

--- practiceset13.py
+++
@@ -1,5 +0,0 @@
-#format the letter using escape sequence characters
-
-letter="Dear harry, This python course is nice. Thanks!"
-letter="Dear harry,\n\tThis python course is nice.\nThanks!"
-print(letter)
